345ba6805c34191cda56948dbcb66e5bc3980320
Andrew Peters <anpete@microsoft.com>
2018-04-10 12:51:28 -0700

    Initial

---
+++ BugBench/Program.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QueryExample.EntityFramework;
+
+namespace BugBench
+{
+    public class Program
+    {
+#pragma warning disable 1998
+        public static async Task Main()
+#pragma warning restore 1998
+        {
+            var context = new QueryExampleContext();
+
+            var userId = 1;
+
+            var competitions = await context.Competitions
+                .Include(c => c.Quest).ThenInclude(q => q.QuestTags)
+                .OrderByDescending(c => c.Passes.Max(qp => qp.CreatedAt))
+                .ToListAsync();
+
+            foreach (var competition in competitions)
+                Console.WriteLine("My friend passed quest '{0}' which has {1} likes", competition.Quest.Name, competition.LikeCount);
+        }
+    }
+}
---
+++ BugBench/QueryExampleContext.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using QueryExample.Entities;
+
+namespace QueryExample.EntityFramework
+{
+    public class QueryExampleContext : DbContext
+    {
+        private static readonly ILoggerFactory _loggerFactory = new LoggerFactory()
+            .AddConsole((s, l) => true);
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserFriend> UserFriends { get; set; }
+        public DbSet<Quest> Quests { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<QuestTag> QuestTags { get; set; }
+        public DbSet<QuestPass> QuestPasses { get; set; }
+        public DbSet<Competition> Competitions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source=(localdb)\\MSSQLLocalDB;Database=BugBench;Integrated Security=True;Connect Timeout=60;ConnectRetryCount=0")
+                .UseLoggerFactory(_loggerFactory);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .ToTable("Users");
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<UserFriend>()
+                .ToTable("UserFriends");
+
+            modelBuilder.Entity<UserFriend>()
+                .HasKey(uf => new { uf.UserId, uf.FriendId });
+
+            modelBuilder.Entity<UserFriend>()
+                .HasOne(uf => uf.User)
+                .WithMany(u => u.UserFriends)
+                .HasForeignKey(uf => uf.UserId);
+
+            modelBuilder.Entity<UserFriend>()
+                .HasOne(uf => uf.Friend)
+                .WithMany()
+                .HasForeignKey(uf => uf.FriendId);
+
+            modelBuilder.Entity<Quest>()
+                .ToTable("Quests");
+
+            modelBuilder.Entity<Tag>()
+                .ToTable("Tags");
+
+            modelBuilder.Entity<Tag>()
+                .HasIndex(t => t.Value)
+                .IsUnique();
+
+            modelBuilder.Entity<QuestTag>()
+                .ToTable("QuestTags");
+
+            modelBuilder.Entity<QuestTag>()
+                .HasKey(qt => new { qt.QuestId, qt.TagId });
+
+            modelBuilder.Entity<QuestPass>()
+                .ToTable("QuestPasses");
+
+            modelBuilder.Entity<Competition>()
+                .ToTable("Competitions");
+
+            modelBuilder.Entity<Competition>()
+                .HasMany(c => c.Passes)
+                .WithOne(qp => qp.Competition)
+                .HasForeignKey(qp => qp.CompetitionId);
+        }
+    }
+}
